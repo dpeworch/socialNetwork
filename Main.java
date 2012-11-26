260386440d4a5f3f8aa51f78c849df4593bd6597
dpeworch <davidpeworchik@gmail.com>
2012-11-26 14:58:44 -0500

    this class is useless now

--- Main.java
+++ Main.java
@@ -1,21 +1,11 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package socialnetwork;
 
-/**
- *
- * @author David
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Brian wuz here");
     }
 
 }
+*/
