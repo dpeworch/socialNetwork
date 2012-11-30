6eeee0a37e6de5124287b73bc3f4d3f2d7437196
dpeworch <dpeworch@mail.umw.edu>
2012-11-29 20:44:35 -0500

    added a few little things

--- User.java
+++ User.java
@@ -1,15 +1,21 @@
+
+package socialnetwork;
+
 import java.util.ArrayList;
 
 
 public class User {
 
-	public User(){
-		
+	public User(int id, String username, String password) {
+            this.id = id;
+            this.username = username;
+            this.password = password;
+            loggedIn = false;
 	}
 	
 	int id;
 	String username;
 	String password;
 	ArrayList<Integer> subscriptions;
-	
+	boolean loggedIn;
 }
