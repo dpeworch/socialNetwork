cd239e5b38ecc5a0fb8f85b2664b42575b68bda6
dpeworch <dpeworch@mail.umw.edu>
2012-11-29 20:49:36 -0500

    added package

--- databasejunk.java
+++ databasejunk.java
@@ -1,16 +1,22 @@
-public boolean contains(int[] ID, Post post) {
-	for (int i = 0; i < ID.length; i++){
-		if (post.userID == ID[i]) {
-			return true;
-		}
-	}
-	return false;
-}
 
-public Post[] makeArrayListofSubs(User user) {
-	
-	// iterate posts
-	// grab posts where post.contains ids
-	// grab last 10.
-	// return last 10.
+package socialnetwork;
+
+public class databasejunk {
+    public boolean contains(int[] ID, Post post) {
+            for (int i = 0; i < ID.length; i++){
+                    if (post.userID == ID[i]) {
+                            return true;
+                    }
+            }
+            return false;
+    }
+
+    public Post[] makeArrayListofSubs(User user) {
+
+        // iterate posts
+        // grab posts where post.contains ids
+        // grab last 10.
+        // return last 10.
+        return null;
+    }
 }
