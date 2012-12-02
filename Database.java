7c597a8c3f9eac2309997f9b017dd0e7819e23d3
dpeworch <dpeworch@mail.umw.edu>
2012-12-01 21:25:49 -0500

    added and commented methods needed for registering and logging in

--- Database.java
+++ Database.java
@@ -2,19 +2,25 @@
 package socialnetwork;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.io.*;
 
 // I am a database
 public class Database {
 
-    String filename = "C://Users/David/NetBeansProjects/socialNetwork/src/socialnetwork/users.txt";
+    String filename = "C://Users/David/NetBeansProjects/socialNetwork/src/socialnetwork/users.txt"; //or whatever the path is
     int maxID = -1;
+
+    ArrayList<Post> posts;
+    ArrayList<User> users;
+    BinaryStringSearchTree tags;
 
     public Database() {
         posts = new ArrayList<Post>();
         users = new ArrayList<User>();
         tags = new BinaryStringSearchTree();
+        addExistingUsers();
     }
 
     // GUI should contain who is currently logged in
@@ -31,43 +37,113 @@ public class Database {
         return tags;
     }
 
-    public void addNewUser(String username, String password) {
+    /**
+     * Reads users.txt so it can add into the ArrayList all users that were already
+     * in the system before the current run of the program.
+     */
+    private void addExistingUsers() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String currentLine = br.readLine();
+            while (!currentLine.equals("")) {
+                Scanner s = new Scanner(currentLine).useDelimiter("\t");
+                maxID ++;
+                int id = Integer.valueOf(s.next());
+                users.add(new User(id, s.next(), s.next()));
+                currentLine = br.readLine();
+            }
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+    /**
+     * Adds a new user into the system.  Both the ArrayList and users.txt are updated.
+     * @param username The new user's username.
+     * @param password The new user's password.
+     * @param email The new user's email address.
+     */
+    public void addNewUser(String username, String password, String email) {
         maxID ++;
         users.add(new User(maxID, username, password));
         try {
-            FileWriter fstream = new FileWriter(filename);
+            PrintWriter fstream = new PrintWriter(new FileWriter(filename, true));
             BufferedWriter out = new BufferedWriter(fstream);
-            out.write(maxID + " " + username + " " + password);
+            out.write(maxID + "\t" + username + "\t" + password + "\t" + email + "\r");
             out.close();
         }
         catch (Exception e) {
             
         }
     }
-    
-    public int findUser(String username, String password) {
-        try {
+
+    /**
+     * Finds a user in the system with the specified username.
+     * @param username The username of the user being searched for.
+     * @return the user with the intended username, or null if none are found
+     */
+    public User findUser(String username) {
+        User current = null;
+        Iterator<User> i = users.listIterator();
+        while (i.hasNext()) {
+            current = i.next();
+            if (current.username.equals(username)) {
+                return current;
+            }
+        }
+        return null;
+        /*try {
             BufferedReader br = new BufferedReader(new FileReader(filename));
             String currentLine = br.readLine();
             while (!currentLine.equals("")) {
-                Scanner s = new Scanner(currentLine).useDelimiter(" ");
+                Scanner s = new Scanner(currentLine).useDelimiter("\t");
                 int id = Integer.valueOf(s.next());
-                if(s.next().equals(username) && s.next().equals(password)) {
+                if (s.next().equals(username)) {
                     return id;
                 }
                 else {
                     currentLine = br.readLine();
                 }
-                //System.out.println(s.next() + " " + s.next() + " " + s.next());
             }
             return -1;
         }
         catch (Exception e) {
             return -1;
-        }
+        }*/
     }
 
-    ArrayList<Post> posts;
-    ArrayList<User> users;
-    BinaryStringSearchTree tags;
+    /**
+     * Validates the user's registration info so the user can be added into the system.
+     * @param username The user's username.
+     * @param password The user's password.
+     * @param email The user's email address.
+     * @return
+     * 0 if they left any fields blank;
+     * 1 if they entered a username that was already in the system;
+     * 2 if their username contained a space;
+     * 3 if their password contained a space;
+     * 4 if their email was in the incorrect form;
+     * 5 if their registration info passed all requirements
+     */
+    public int validateRegistrationInfo(String username, String password, String email) {
+        if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+            return 0;
+        }
+        else if (findUser(username) != null) {
+            return 1;
+        }
+        else if (!username.matches("^[A-Za-z0-9]+$")) {
+            return 2;
+        }
+        else if (!password.matches("^[A-Za-z0-9]+$")) {
+            return 3;
+        }
+        else if (!email.matches("^[A-Za-z0-9]+@[A-Za-z0-9]+(\\.[A-Za-z]{2,})$")) {
+            return 4;
+        }
+        else {
+            return 5;
+        }
+    }
 }
