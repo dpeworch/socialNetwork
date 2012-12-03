6ad2a73a57a319b6f4eaf5299ef0efd2a7412557
Elisabeth <ena1234@hotmail.com>
2012-12-03 00:10:13 -0500

    stringnode
    trying to commit stringnode...

--- StringNode.java
+++ StringNode.java
@@ -3,28 +3,35 @@ package socialnetwork;
 
 import java.util.ArrayList;
 
-
+/**
+ * 
+ * @author Elisabeth
+ * This is a container class for a binary tree that holds a string.
+ */
 public class StringNode {
-
-	public StringNode(){
-		
-		posts = new ArrayList<Post>();
-		left = null;
-		right = null;
-	}
-	
-	public void addNode(boolean l, StringNode toAdd){
-		if (l){
-			left = toAdd; 
-		}
-		else {
-			right = toAdd;
-		}
-	}
-	
-	ArrayList<Post> posts;
+	String tag;
+	ArrayList<Post> data;
 	StringNode left;
 	StringNode right;
 	
 	
+	/**
+	 * The default constructor.
+	 */
+	public StringNode(){
+		data = new ArrayList<Post>();
+		left = null;
+		right = null;
+	}
+	
+	public StringNode(String s){
+		tag = s;
+		data = new ArrayList<Post>();
+		left = null;
+		right = null;
+	}
+	
+	public void addPost(Post post){
+		data.add(post);
+	}
 }
