396730053471eec5f79865b8736ee613782c7938
dpeworch <dpeworch@mail.umw.edu>
2012-12-02 20:08:23 -0500

    forgot getEmail method

--- User.java
+++ User.java
@@ -9,13 +9,15 @@ public class User {
     private int id;
     private String username;
     private String password;
+    private String email;
     private ArrayList<Integer> subscriptions;
 
-    public User(int id, String username, String password) {
+    public User(int id, String username, String password, String email, ArrayList<Integer> subscriptions) {
         this.id = id;
         this.username = username;
         this.password = password;
-        subscriptions = new ArrayList<Integer>();
+        this.email = email;
+        this.subscriptions = subscriptions;
     }
 
     public int getUserId() {
@@ -28,6 +30,10 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public ArrayList<Integer> getSubsAsArrayList() {
@@ -49,12 +55,14 @@ public class User {
         }
         return subs;
     }
-    
+
+    //moved to database
     public void addSub(int subid){
         subscriptions.ensureCapacity(subscriptions.size() + 1);
         subscriptions.add(subid);
     }
-    
+
+    //moved to database
     public void addManySubs(int[] subids){
         subscriptions.ensureCapacity(subscriptions.size() + subids.length);
         for(int i=0; i<subids.length; i++){
